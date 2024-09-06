1bbebf9ea9e81fa8a4e8118cdde5552948e2d20e
Du_dudah <126823078+MdDud04@users.noreply.github.com>
2024-09-06 12:51:53 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,57 @@
+let dados = [
+    {
+        titulo : "Basquete com cadeira de rodas",
+        descricao : "O basquete em cadeira de rodas é uma modalidade paralímpica que combina velocidade, estratégia e trabalho em equipe. Originado como forma de reabilitação para veteranos de guerra, o esporte conquistou o mundo e faz parte dos Jogos Paralímpicos desde sua primeira edição.",
+        link : "https://cpb.org.br/modalidades/basquete-em-cr/",
+    },
+
+    {
+        titulo : "Natacao Paralímpica",
+        descricao : "A natação paralímpica é uma das modalidades mais populares e inclusivas dos Jogos Paralímpicos. Com atletas de diversas classes funcionais, a modalidade desafia os limites da performance humana.",
+        link : "https://cpb.org.br/modalidades/natacao/"
+    },
+
+    {
+        titulo : "Atletismo Paralímpico",
+        descricao : "O atletismo paralímpico é uma das modalidades mais inspiradoras dos Jogos Paralímpicos. Com uma variedade de provas, desde corridas até arremessos, essa modalidade permite que atletas com diferentes tipos de deficiência demonstrem força, agilidade e determinação.",
+        link : "https://cpb.org.br/modalidades/atletismo/"
+    },
+
+    {
+        titulo : "Bocha",
+        descricao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        link : "https://cpb.org.br/modalidades/bocha/"
+    },
+
+    {
+        titulo : "Vôlei Sentado",
+        descricao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        link : "https://cpb.org.br/modalidades/volei-sentado/"
+    },
+
+    {
+        titulo : "Canoagem",
+        descricao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        link : "https://cpb.org.br/modalidades/canoagem/"
+    },
+
+    {
+        titulo : "Halterofilismo",
+        descricao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        link : "https://cpb.org.br/modalidades/halterofilismo/"
+    },
+
+    {
+        titulo : "Tênis de Mesa",
+        descricao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        link : "https://cpb.org.br/modalidades/tenis-de-mesa/"
+    },
+
+    {
+        titulo : "Ciclismo",
+        descricao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        link : "https://cpb.org.br/modalidades/ciclismo/"
+    }
+];
+
+
